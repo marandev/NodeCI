b01c835e7660963c3a9ccb00d347cdb910bbd030
Mac Gutierrez <mac.gutierrez@grapikom.com>
2020-06-10 13:49:52 +0800

    Node CI Travis push

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '406862809864-9boq1kl1sqna2vustg8ev5ir0iccemut.apps.googleusercontent.com',
+  googleClientSecret: 'JrP9OOFTi7PY5y3p1NtTm9WG',
+  mongoURI: 'mongodb://localhost/blog_everyone',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
+};
